bd3974c946755137df013e3844684f743dd2850b
Mr. Lei <53390868+wuyilingwei@users.noreply.github.com>
2024-11-15 11:37:17 -0500

    Update

--- SatisfactoryDedicatedServer/Game download update file.bat
+++ SatisfactoryDedicatedServer/Game download update file.bat
@@ -1,1 +1,6 @@
-taskkill /IM FactoryServer-Win64-Shipping-Cmd.exe /F & start C:\SatisfactoryDedicatedServer\steamcmd\steamcmd.exe +force_install_dir C:\SatisfactoryDedicatedServer\GameServers\SatisfactoryServer +login anonymous +app_update 1690800 -beta public validate +quit
+if not exist "%~dp0steamcmd" (
+    mkdir "%~dp0steamcmd"
+    copy /Y "%~dp0steamcmd.exe" "%~dp0steamcmd\steamcmd.exe"
+)
+taskkill /IM FactoryServer-Win64-Shipping-Cmd.exe /F
+"%~dp0steamcmd/steamcmd.exe" +force_install_dir "%~dp0SatisfactoryServer" +login anonymous +app_update 1690800 -beta public validate +quit
--- SatisfactoryDedicatedServer/start.bat
+++ SatisfactoryDedicatedServer/start.bat
@@ -1,20 +1,43 @@
 @echo off
-echo You are starting the SatisfactoryDedicatedServer in daemon mode
 
-taskkill /f /t /im FactoryServer-Win64-Shipping-Cmd.exe
+title checking for admin rights
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+if '%errorlevel%' NEQ '0' (
+title waiting for admin rights
+mode con cols=20 lines=1
+goto UACPrompt
+) else ( goto start )
+:UACPrompt
+echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+"%temp%\getadmin.vbs"
+exit /B
+:start
+
+title FactoryServerDaemon
+echo You are starting the SatisfactoryDedicatedServer in daemon mode
+echo Please do not close this window
+
+taskkill /f /t /im FactoryServer-Win64-Shipping-Cmd.exe >nul 2>&1
+
+"%~dp0satisfactoryserver/FactoryServer.exe" -log -unattended -port=7777
 
 setlocal
- 
+
+setlocal EnableDelayedExpansion
+
 set PORT=7777
- 
+
 :check_port
 set "found="
 for /F "tokens=2 delims=:" %%a in ('netstat -a -n -o -p UDP ^| findstr :%PORT%') do set "found=%%a"
 if not defined found (
     echo %date%%time% SatisfactoryDedicatedServer is offline. Restarting the server, please be patient
-taskkill /f /t /im FactoryServer-Win64-Shipping-Cmd.exe & start C:\SatisfactoryDedicatedServer\GameServers\SatisfactoryServer\FactoryServer.exe -log -unattended -port=7777
+    nircmd stopprocess "FactoryServer-Win64-Shipping-Cmd.exe"
+    timeout /T 10 >nul
+    "%~dp0satisfactoryserver/FactoryServer.exe" -log -unattended -port=7777
 )
- 
-echo %date%%time% SatisfactoryDe​​dicatedServer is working.  QQ:1056484009 QQgroup:264127585 
+echo %date%%time% SatisfactionDedicatedServer is working properly. QQ:1056484009 QQgroup:264127585
+
 timeout /T 30 >nul
 goto check_port
